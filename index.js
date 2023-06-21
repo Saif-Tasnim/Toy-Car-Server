5944ea42e991a0354996b8cb9605ccc7bbe3853d
Saif-Tasnim <saiftasnim2002@gmail.com>
2023-06-21 19:32:27 +0600

    initial setup ongoing

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
