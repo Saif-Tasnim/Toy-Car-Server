619c5c2f765f6d576d55da198f8ff8409cdfc116
Saif-Tasnim <saiftasnim2002@gmail.com>
2023-06-21 21:12:24 +0600

    basic routes setup

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const port = process.env.PORT || 5000;
 require('dotenv').config();
 
 
@@ -7,3 +8,12 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+
+app.get('/' , (req,res) => {
+    res.send("Toys Car site server is open");
+})
+
+app.listen(port , ()=>{
+    console.log("Server in background successfully running");
+})
