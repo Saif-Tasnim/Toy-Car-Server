671337440ffcd2f6061ac97989cea661c9b5fa58
Saif-Tasnim <saiftasnim2002@gmail.com>
2023-06-23 11:49:15 +0600

    server side has query got

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
@@ -13,7 +14,7 @@ app.use(express.json());
 // mongodb start
 
 
-const uri = "mongodb+srv://<username>:<password>@cluster0.ectfhk2.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ectfhk2.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -31,6 +32,21 @@ async function run() {
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    const toy_car = client.db("toy_car");
+    const carCollection = toy_car.collection("carData");
+
+    app.get('/', (req, res) => {
+      res.send("Toys Car site server is open");
+    })
+
+    app.get('/carData', (req, res) => {
+      const query = req.query.product;
+
+      console.log(query);
+
+    })
+
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -42,10 +58,8 @@ run().catch(console.dir);
 // mongodb ends
 
 
-app.get('/' , (req,res) => {
-    res.send("Toys Car site server is open");
-})
 
-app.listen(port , ()=>{
-    console.log("Server in background successfully running");
+
+app.listen(port, () => {
+  console.log("Server in background successfully running");
 })
